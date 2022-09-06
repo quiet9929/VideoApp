8ddb7600c161c4f836ccf1710a8ee8c01c7cf744
quiet9929 <542895239@qq.com>
2022-09-06 20:02:22 +0800

    updated ...

--- VideoApp/Pages/VideoListPage.xaml.cs
+++ VideoApp/Pages/VideoListPage.xaml.cs
@@ -1,12 +1,15 @@
+using VideoApp.Models;
 using VideoApp.ViewModels;
 
 namespace VideoApp.Pages;
 
 public partial class VideoListPage : ContentPage
 {
-	public VideoListPage()
-	{
-		InitializeComponent();
-		BindingContext = new VideoListViewModel() { Navigation = Navigation };
-	}
+
+    public VideoListPage()
+    {
+        InitializeComponent();
+        BindingContext = new VideoListViewModel() { Navigation = Navigation };
+    }
+
 }
--- VideoApp/ViewModels/VideoListViewModel.cs
+++ VideoApp/ViewModels/VideoListViewModel.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using VideoApp.Models;
 
 namespace VideoApp.ViewModels
@@ -16,8 +17,8 @@ namespace VideoApp.ViewModels
 
         public INavigation Navigation { get; set; }
 
-        public IList<Video> VideoList { get; set; } = new List<Video>() 
-        { 
+        public IList<Video> VideoList { get; set; } = new List<Video>()
+        {
             new Video() { Name = "古惑仔1" },
             new Video() { Name = "古惑仔2" },
             new Video() { Name = "古惑仔3" },
@@ -29,18 +30,39 @@ namespace VideoApp.ViewModels
             new Video() { Name = "古惑仔9" },
         };
 
-        public IList<string> VideoTypes { get; set; } = new List<string>() { "电视剧", "电影", "综艺", "纪录片" };
+        private IList<VideoTypeTab> _videoTypeTabs = new List<VideoTypeTab>();
+        public IList<VideoTypeTab> VideoTypeTabs { 
+            get { return _videoTypeTabs; } 
+            set { _videoTypeTabs = value; NotifyPropertyChanged(); }
+        } 
 
-        public VideoListViewModel() 
+        public ICommand OnVideoTypeTabClicked { get; set; }
+        public VideoListViewModel()
         {
-         
+            OnVideoTypeTabClicked = new Command<string>(VideoTypeTabClicked);
+            VideoTypeTabs = new List<VideoTypeTab>()
+            {
+                new VideoTypeTab() { Name = "电视剧", Active = true },
+                new VideoTypeTab() { Name = "电影" },
+                new VideoTypeTab() { Name = "综艺" },
+                new VideoTypeTab() { Name = "纪录片" },
+            };
         }
 
-        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
         {
             Console.WriteLine($"NotifyPropertyChanged = {propertyName}");
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-    
+
+        private void VideoTypeTabClicked(string name)
+        {
+            Console.WriteLine($"name={name}");
+            foreach (var tab in VideoTypeTabs)
+            {
+                tab.Active = tab.Name == name;
+            }
+        }
+
     }
 }
